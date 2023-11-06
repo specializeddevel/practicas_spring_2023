14a6cda8045bb333619698489b004fb2b47b9678
Raul Burgos Murray <specializeddevel@gmail.com>
2023-11-06 14:58:45 -0400

    First commit

---
+++ Projects/firstapp-sboot/src/main/java/com/springfirst/firstappsboot/FirstappSbootApplication.java
@@ -0,0 +1,13 @@
+package com.springfirst.firstappsboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstappSbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstappSbootApplication.class, args);
+	}
+
+}
---
+++ Projects/firstapp-sboot/src/test/java/com/springfirst/firstappsboot/FirstappSbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springfirst.firstappsboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstappSbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
