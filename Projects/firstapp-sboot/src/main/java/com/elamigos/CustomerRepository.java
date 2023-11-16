b9dfb56865cc8cc2dbcc3c36c39da9128c0e4b1d
Raul Burgos Murray <specializeddevel@gmail.com>
2023-11-15 20:40:45 -0400

    Proyecto finalizado

---
+++ Projects/firstapp-sboot/src/main/java/com/elamigos/CustomerRepository.java
@@ -0,0 +1,15 @@
+package com.elamigos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository
+        extends JpaRepository<Customer, Integer> {
+
+    @Query("SELECT c FROM Customer c WHERE c.name = :name")
+    public Customer searchCustomerByName(@Param("name") String name);
+
+}
--- Projects/firstapp-sboot/src/main/java/com/elamigos/Main.java
+++ Projects/firstapp-sboot/src/main/java/com/elamigos/Main.java
@@ -1,43 +1,102 @@
 package com.elamigos;
 
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
+/**
+ * Proyecto para aprendizaje de Spring Boot e Spring data
+ * @author raulb
+ * @version 1.0
+ */
 @SpringBootApplication
 //@SpringBootApplication hace en una sola linea tod o lo que hacen las siguientes anotations
 //@ComponentScan(basePackages = "com.elamigos")
 //@EnableAutoConfiguration
-//@Configuration
+//@SprigBootConfiguration
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+    //Inicio de la Inyeccion de dependencias mediante constructor
+    private final CustomerRepository customerRepository;
 
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    //Esta forma de inyectar las dependencias tambien es valida
+    //No se neceista instanciar el objeto, el @Autowired lo hace automaticamente
+    //@Autowired
+    //private CustomerRepository customerRepository2;
+    //Fin de la inmyeccion de dependencias mediante constructor
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet(){
-        return new GreetResponse(
-                "Hello",
-                List.of("Java", "Golang", "JavaScript"),
-                new Person("Alex", 28, 30_000)
-        );
-
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return customerRepository.findAll();
     }
 
-    record Person(String name, int age, double savings){}
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
     ){}
+
+    @GetMapping("{customerName}")
+    public Customer searchCustomersByName(@PathVariable("customerName") String name){
+        return customerRepository.searchCustomerByName(name);
+    }
+
+    @PostMapping
+    @Transactional
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    @Transactional
+    public void updateCustomer(@PathVariable("customerId") Integer id, @RequestBody NewCustomerRequest request){
+
+        Optional<Customer> optionalCustomer = customerRepository.findById(id);
+        optionalCustomer.ifPresent(customer -> {
+            customer.setName(request.name());
+            customer.setEmail(request.email());
+            customer.setAge(request.age());
+            customerRepository.save(customer);
+        });
+    }
+
+//    @GetMapping("/greet")
+//    public GreetResponse greet(){
+//        return new GreetResponse(
+//                "Hello",
+//                List.of("Java", "Golang", "JavaScript"),
+//                new Person("Alex", 28, 30_000)
+//        );
+//
+//    }
+//
+//    record Person(String name, int age, double savings){}
+//    record GreetResponse(
+//            String greet,
+//            List<String> favProgrammingLanguages,
+//            Person person
+//    ){}
 
 }
